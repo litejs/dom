7f024f6fbb34c589f350b78f5fc28c26857353aa
Lauri Rooden <lauri@rooden.ee>
2015-02-06 23:12:12 +0200

    Add Node constants

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ function getSibling(node, step) {
 }
 
 Node.prototype = {
+	ELEMENT_NODE:                1,
+	TEXT_NODE:                   3,
+	PROCESSING_INSTRUCTION_NODE: 7,
+	COMMENT_NODE:                8,
+	DOCUMENT_NODE:               9,
+	DOCUMENT_TYPE_NODE:         10,
+	DOCUMENT_FRAGMENT_NODE:     11,
 	nodeName:        null,
 	parentNode:      null,
 	ownerDocument:   null,
