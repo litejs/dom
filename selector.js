b94f31a3f262a926b0a15f0ef7133831a4e4136c
Lauri Rooden <lauri@rooden.ee>
2015-02-19 22:57:11 +0200

    Release v0.1.0

--- selector.js
+++ selector.js
@@ -1,8 +1,8 @@
 
 
 /**
- * @version    0.3.10
- * @date       2015-02-05
+ * @version    0.1.0
+ * @date       2015-02-19
  * @stability  2 - Unstable
  * @author     Lauri Rooden <lauri@rooden.ee>
  * @license    MIT License
