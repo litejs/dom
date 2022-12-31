prantlf/dom-lite

Add element getters to document fragment

index.js
@@ -254,7 +254,7 @@ function DocumentFragment() {
 	this.childNodes = []
 }
 
-extendNode(DocumentFragment, {
+extendNode(DocumentFragment, elementGetters, {
 	nodeType: 11,
 	nodeName: "#document-fragment"
 })

litejs/dom

index.js
@@ -272,7 +272,7 @@ function DocumentFragment() {
 	this.childNodes = []
 }
 
-extendNode(DocumentFragment, {
+extendNode(DocumentFragment, Element, {
 	nodeType: 11,
 	nodeName: "#document-fragment"
 })